b0b5af38e8e7e94f77b0ecf76938fe3a93868945
Nastya Ladynskaya <feverrr27@gmail.com>
2022-10-01 13:32:50 +0300

    styles

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,14 +1,38 @@
 import React from 'react'
+import { FaSearch } from "react-icons/fa";
+
 
 const Search = () => {
   return (
     <div className="search">
-        <h2>Find Your Book Of Choice</h2>
-        <div className="search-form">
-            
+      <h2>Find Your Book Of Choice</h2>
+      <div className="search-form">
+        <div className="container">
+          <div className="search-form-content">
+            <form className="search-form" 
+            // onSubmit={handleSubmit}
+            >
+              <div className="search-form-elem flex flex-sb bg-white">
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="The Lost World ..."
+                //   ref={searchText}
+                />
+                <button
+                  type="submit"
+                  className="flex flex-c"
+                //   onClick={handleSubmit}
+                >
+                  <FaSearch className="text-purple" size={32} />
+                </button>
+              </div>
+            </form>
+          </div>
         </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Search
